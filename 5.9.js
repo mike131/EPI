d6c1386c4a84611cece77016f918a39a11792ac9
Mike Lee <mikelee131@gmail.com>
2016-11-14 11:47:32 -0800

    Add 5.9 problem

---
+++ 5.9.js
@@ -0,0 +1,24 @@
+/**
+ * Created by mlee on 11/14/16.
+ * Check if a decimal integer is a palindrome
+ */
+
+function isPalindrome(testInt) {
+    if (testInt < 0) {
+        // Negative decimal cannot be a palindrome, due to  starting with a -
+        return false;
+    } else if (testInt === 0) {
+        // According to problem question, 0 is considered a palindrome
+        return true;
+    }
+
+    // To test if a decimal integer is a palindrome, we can simply compare it to it's reverse
+    // Convert both to string (not necessary, we can also just do a == comparison)
+    if (testInt.toString() === testInt.toString().split("").reverse().join("")) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+// What is BigO ? (Largely relies on what the built in string functions "cost"
